2aaa23393a7214fe379fbc6a16b8e63e740a2460
ModzOn <modzongaming@gmail.com>
2017-12-17 13:50:18 +0100

    request class

--- src/rest/request.js
+++ src/rest/request.js
@@ -1,47 +1,47 @@
-const snekfetch = require("snekfetch");
-const logger = require("../util/Logger");
-const { userAgent } = require("../Constants");
+const request = require("snekfetch");
+const { USER_AGENT } = require("../Constants");
 const { parseString } = require("xml2js");
 
-async function getNation() {
-  try {
-    const result = await snekfetch
-      .get("https://www.nationstates.net/cgi-bin/api.cgi?q=happenings;filter=founding;limit=5")
-      .set({ "User-Agent": userAgent });
-    let nation;
-    parseString(result.text, (err, obj) => {
-      if (err) throw err;
-      const raw = obj.WORLD.HAPPENINGS[0].EVENT[0];
-      const text = raw.TEXT[0].split("@@").join("");
-      [nation] = text.trim().split(" ");
-    });
-    return nation;
-  } catch (error) {
-    logger.error("NSAPI", error.stack);
-    return null;
-  }
-}
+module.exports = class Request {
+  static getNation() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await request
+          .get("https://www.nationstates.net/cgi-bin/api.cgi?q=happenings;filter=founding;limit=5")
+          .set({ "User-Agent": USER_AGENT });
 
-function sendTG({ clientid, tgid, secret, nation } = {}) {
-  return new Promise(async (resolve, reject) => {
-    if (!clientid || !tgid || !secret || !nation || nation === null) reject(new RangeError("PARAMS_MISSING"));
-    try {
-      const res = await snekfetch
-        .get(`https://www.nationstates.net/cgi-bin/api.cgi?a=sendTG&client=${clientid}&tgid=${tgid}&key=${secret}&to=${nation}`)
-        .set({ "User-Agent": userAgent });
-
-      if (res.status !== 200) {
-        reject(res.text);
+        if (result.status !== 200) {
+          reject(result.text);
+        }
+        let nation;
+        parseString(result.text, (err, obj) => {
+          if (err) throw err;
+          const raw = obj.WORLD.HAPPENINGS[0].EVENT[0];
+          const text = raw.TEXT[0].split("@@").join("");
+          [nation] = text.trim().split(" ");
+        });
+        resolve(nation);
+      } catch (error) {
+        reject(error);
       }
-      resolve({ status: res.text, code: res.status });
-    } catch (error) {
-      logger.error("NSAPI", error);
-      reject(error);
-    }
-  });
-}
+    });
+  }
 
-module.exports = {
-  sendTG,
-  getNation
+  static sendTG({ clientID, tgID, secret, nation } = {}) {
+    return new Promise(async (resolve, reject) => {
+      if (!clientID || !tgID || !secret || !nation || nation === null) reject(new RangeError("PARAMS_MISSING"));
+      try {
+        const res = await request
+          .get(`https://www.nationstates.net/cgi-bin/api.cgi?a=sendTG&client=${clientID}&tgid=${tgID}&key=${secret}&to=${nation}`)
+          .set({ "User-Agent": USER_AGENT });
+
+        if (res.status !== 200) {
+          reject(res.text);
+        }
+        resolve({ status: res.text.includes("\n") ? res.text.replace("\n", "") : res.text, code: res.status });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 };
